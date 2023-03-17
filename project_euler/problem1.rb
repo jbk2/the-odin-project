b2ed8edf089c1c04d31dfc598dfef53b4bc1795a
jbk2 <james@humanbenefits.com>
2023-03-17 15:59:17 +0100

    Write ProjectEuler #multiples_sum & #factorial

---
+++ project_euler/problem1.rb
@@ -0,0 +1,18 @@
+def multiples_sum(divisor1, divisor2, ceiling)
+  divisor1_results = (1..ceiling).select { |n| n % divisor1 == 0 }
+  divisor2_results = (1..ceiling).select { |n| n % divisor2 == 0 }
+  divisor1_results.sum + divisor2_results.sum
+end
+# puts multiples_sum(3,5,100)
+
+def factorial(n, total=0)
+  while n > 1
+    total > 0 ? total = total * (n-1) : total += n * (n-1)
+    n -= 1
+    factorial(n, total)
+  end
+  total
+end
+
+
+puts factorial(5)
