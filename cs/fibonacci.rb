2adc4937d61ecb2172fe7e178ec409ae2f6058f5
jbk2 <james@humanbenefits.com>
2023-03-17 09:03:25 +0100

    In #fibs(n) cater for n < 2

--- cs/fibonacci.rb
+++ cs/fibonacci.rb
@@ -1,16 +1,23 @@
 def fibs(n) # while loop version
   result = [0, 1]
-  while n > result.length
-    result << result[-1] + result [-2]
+  if n == 1
+    return result[n-1]
+  elsif n == 0  
+    return
+  else
+    while n > result.length
+      result << result[-1] + result [-2]
+    end
   end
   result
 end
 
+# Have not catered for n < 2 in this case
 def fibs_rec(n, result=[0, 1])
   result << (result[-1] + result[-2])
   n -= 1
   n > 2 ? fibs_rec(n, result) : result
 end
 
-puts fibs(8)
-puts fibs_rec(12)
+puts fibs(23)
+# puts fibs_rec(12)
