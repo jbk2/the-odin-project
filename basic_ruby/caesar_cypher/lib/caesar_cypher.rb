05845533f3fd9326116e49625845c5fabaa521ab
jbk2 <james@humanbenefits.com>
2023-04-18 07:34:07 +0200

    Tidy Caesar Cypher

--- basic_ruby/caesar_cypher/lib/caesar_cypher.rb
+++ basic_ruby/caesar_cypher/lib/caesar_cypher.rb
@@ -37,65 +37,8 @@ def cypher(string, factor)
   puts cyphered_array_alpha.join
 end
 
-# cypher('abcdz', 9)
-
-# p alpha_index_hash
-
 ord_array = [] 
 
 ('A'..'Z').to_a.each do |item|
   ord_array << item.ord
 end
-
-puts ord_array
-
-
-# def yell_greeting(string)
-#   name = string
-
-
-#   name = name.upcase
-#   greeting = "WASSAP, #{name}!"
-#   puts greeting
-# end
-
-# yell_greeting("bob")
-
-# --------------------------- -----------------------------
-
-# def isogram?(string)
-#   original_length = string.length
-#   string_array = string.downcase.split
-
-#   binding.pry
-
-#   unique_length = string_array.uniq.length
-#   original_length == unique_length
-# end
-
-# isogram?("Odin")
-
-# --------------------------- -----------------------------
-
-# string1 = "fewchars"
-# string2 = "above10chars"
-
-# def cap_if_above_10(string)
-# 	string.length > 10 ? puts(string.upcase) : puts(string)
-# 	# puts string
-# end
-
-# # cap_if_above_10(string1)
-
-# # cap_if_above_10(string2)
-
-
-# def number_range(input)
-# 	answer = input.to_i
-# end
-
-# case number_range(gets.chomp)
-# when 0..50 		then puts("your input was between 0 and 50")
-# when 51..100 	then puts("your input was between 51 and 100")
-# else 					puts("your input was above 100")
-# end
